fc80f11e240d0e366370dd36da816ebd790a11c6
Jonathan C MacMillan <jmacmill@gmail.com>
2020-07-08 09:23:31 +1000

    Burning Airlines first component template start

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import BurningAirlines from './BurningAirlines'
+
+
+function App() {
+  return (
+    <div className="App">
+      <BurningAirlines />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/BurningAirlines.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+// import axios from 'axios';
+
+
+class BurningAirlines extends Component {
+  render () {
+    return (
+      <div>
+        <h1>Welcome to Burning Airlines</h1>
+        <p>fly at your own risk</p>
+        <Login />
+        <Locations />
+        <Flights />
+        <Reservations />
+        <SeatMap />
+        <MyRes />
+      </div>
+    )
+  }
+
+
+
+
+
+}
+//
+// <BurningAirlines />
+//   <div/>
+//     <Title/>         // not needed , this gets absorbed by burning airlines
+//     <Login />
+//     <Locations />
+//     <Flights />
+//     <Reservations />
+//     <SeatMap />
+//     <MyRes />
+//   <div/>
