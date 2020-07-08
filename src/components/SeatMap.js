12ff01f134b4da02de4199dc908ca5653c4dd597
BlakeDavis28 <blake_davis@outlook.com>
2020-07-08 14:32:00 +1000

    seatmap 1

--- src/components/SeatMap.js
+++ src/components/SeatMap.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
+const FLIGHTS_URL = 'http://localhost:3000/flights.json';
 
 class Seat extends Component {
   checkTaken = () => {
@@ -59,8 +62,32 @@ class SeatMap extends Component {
 
   render() {
     return (
-
-    )
+      <div>
+        <h2>Seat Map</h2>
+        <p>Please choose an available seat</p>
+        <div className="grid-container" >
+          {/* make a row for number of rows */}
+          { [...Array(this.props.rows)].map((e, i) =>
+            <div className="grid-row" key={i}>
+              {/* row letter */}
+              <span className="row-num">{ String.fromCharCode(i+65) }</span>
+              {/*  make seat / num of cols */}
+              { [...Array(this.props.cols)].map((e, j) =>
+                <Seat
+                  key={`${String.fromCharCode(i+65)}${j+1}`}
+                  seatId={`${String.fromCharCode(i+65)}${j+1}`}
+                  takenSeats={ this.props.takenSeats }
+                  getSelectedSeat={ this.getSelectedSeat }
+                  selectedSeat={ this.state.selectedSeat }
+                />
+              ) }
+            </div>
+          ) }
+        </div>
+        <p>Selected Seat: { this.state.selectedSeat } </p>
+      </div>
+    );
   }
 }
+
 export default SeatMap;
