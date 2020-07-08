83b4126214142cd6ade5bd7f14d53311894aa48c
jnielsen85 <thisisjn@gmail.com>
2020-07-08 16:21:12 +1000

    handlers added

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,21 +4,55 @@ import axios from 'axios';
 const FLIGHTS_URL = 'http://localhost:3000/flights.json';
 
 class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      origin: '',
+      destination: ''
+    };
+  }
+    _handleChange1(event){
+      console.log(event.target.value);
+      this.setState( {origin: event.target.value} );
+    }
+
+    _handleChange2(event){
+      console.log(event.target.value);
+      this.setState( {destination: event.target.value} );
+    }
+
+    _handleSubmit(event) {
+        event.preventDefault();
+        this.props.onSubmit(this.state.origin, this.state.destination);
+      }
+
   render() {
     return (
-      <div>
-        <form onSubmit={ this.handleSubmit }>
-          <label> Origin:
-            <input type="search" placeholder="Origin" />
-          </label><br/>
+      <form onSubmit={ this.handleSubmit }>
 
-          <label> Destination:
-            <input type="search" placeholder="Destination" />
-          </label><br />
+        <label className = "search"><span>
+        Origin: &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  </span>
+          <select value={this.state.origin} onChange={this._handleChange1}>
+            <option value="">Select...</option>
+            <option value="Sydney">Sydney</option>
+            <option value="Brisbane">Brisbane</option>
+            <option value="Adelaide">Adelaide</option>
+          </select>
+        </label><br />
 
-          <input type="submit" value="Search" />
-        </form>
-      </div>
+        <label className = "search"><span>
+        Destination: &nbsp; &nbsp; &nbsp;</span>
+          <select value={this.state.destination} onChange={this._handleChange2}>
+            <option value="">Select...</option>
+            <option value="Sydney">Sydney</option>
+            <option value="Brisbane">Brisbane</option>
+            <option value="Adelaide">Adelaide</option>
+          </select>
+        </label><br />
+
+
+        <input type="submit" value="Search" />
+      </form>
     );
   }
 }
