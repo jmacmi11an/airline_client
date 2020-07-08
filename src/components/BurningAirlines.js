114b69b53047cd66e278cbdebb0b8b7de27158ef
Jonathan C MacMillan <jmacmill@gmail.com>
2020-07-08 22:26:28 +1000

    updates to login and reservations

--- src/components/BurningAirlines.js
+++ src/components/BurningAirlines.js
@@ -3,6 +3,7 @@ import Flights from './Flights'
 import Search from './Search'
 import SeatMap from './SeatMap';
 import Reservations from './Reservations';
+import Login from './Login';
 // import axios from 'axios';
 
 
@@ -18,54 +19,14 @@ class BurningAirlines extends Component {
       <div>
         <h1>Welcome to Burning Airlines</h1>
         <p>fly at your own risk</p>
-         <Login />
          <Search />
          <Flights flights={this.props.flights}/>
          <SeatMap />
+         <Login />
          <Reservations />
       </div>
     )
   }
-}
-
-
-
-class Login extends Component {
-  constructor(){
-    super();
-
-    this.state = {
-      email: '',
-      passwordDigest: '',
-    }
-
-    this._handleInput = this._handleInput.bind(this);
-    this._handleSubmit = this._handleSubmit.bind(this);
-  }
-
-  _handleInput(event){
-    this.setState({email: event.target.value});
-    console.log('handleinput event', event.target.value)
-  }
-
-  _handleSubmit(event){
-    event.preventDefault();
-    console.log('handlesubmit event', event)
-  }
-  render(){
-    return(
-      <div>
-        <form onSubmit={ this._handleSubmit }>
-          <label>Email:</label>
-          <input name="email" type="text" placeholder="john.doe@gmail.com" required onInput={ this._handleInput }/>
-          <label>Password: </label>
-          <input name="password" type="password" required/>
-          <button>Submit</button>
-        </form>
-      </div>
-    )
-  }
-
 }
 
 
--- src/components/Reservations.js
+++ src/components/Reservations.js
@@ -8,14 +8,40 @@ class Reservations extends Component {
     super();
     this.state = {
       reservations: [],
-      userID: null,
+      userID: "1",    //testing - otherwise start as null
+      myRes: [] //myReservations(),
     }
+
+    const fetchReservations = () => {
+      axios.get(RESERVATIONS_URL).then((results) => {
+        this.setState({reservations: results.data})
+      })
+    }
+
   }
+
+//this function returns an array of objects that match the user with this.state.userID
+// const myReservations = () => {
+//   let result = [];
+//   let allReservations = this.state.reservations
+//   for (let i = 0; i < allReservations.length; i++){
+//     if (allReservations[i].user_id == this.state.userID){
+//       result.push(allReservations[i])
+//     }
+//     }
+//     console.log(result);
+//     return result
+//   }
+
 
   render (){
     return (
       <div>
         <h4>My reservations:</h4>
+        { this.state.myRes.map((r) =>
+          <ul>
+            <li>{r.seat} - {r.url}</li>
+          </ul>)}
 
       </div>
     )
@@ -27,3 +53,8 @@ class Reservations extends Component {
 
 
 export default Reservations;
+
+
+
+
+// testing a few functions to pull things from array of objects:
